4eacba7f4ad6f508d40e872bb380d76a842ebacf
TajgramRobot <robot@tajgram.tj>
2026-06-28 20:18:25 +0000

    Tajgram: Auto-updated Security & API

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -29548,6 +29548,8 @@ public class ChatActivity extends BaseFragment implements
     }
     // === TAJGRAM SCREENSHOT LOCK END ===
 
+    
+
     // === TAJGRAM SCREENSHOT LOCK START (DO NOT DUPLICATE) ===
     if (org.telegram.messenger.BuildVars.ANTI_SPY_SCREENSHOT_LOCK) {
         if (getParentActivity() != null) {

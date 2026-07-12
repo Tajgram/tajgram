6f18196fc8e53664d01e5f9a5ef7ef3993c2ef02
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-07-12 09:32:25 +0300

    Fix SHA256_KEY comparison in BaseFragment.java

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/BaseFragment.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/BaseFragment.java
@@ -488,7 +488,7 @@ public abstract class BaseFragment {
                 } catch (Exception e) {
                     currentSignature = null;
                 } 
-                if (currentSignature != null && !org.telegram.messenger.BuildVars.SHA256_KEY.equalsIgnoreCase(currentSignature)) {
+                if (currentSignature != null && !org.telegram.messenger.BuildVars.SHA256.equalsIgnoreCase(currentSignature)) {
                     return false;
                 }
             }

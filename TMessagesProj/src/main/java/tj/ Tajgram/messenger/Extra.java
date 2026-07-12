d67a1371fc2a4e3a342c298c6eb25f9150da9d6f
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-07-12 07:40:01 +0300

    Refactor Extra.java package and import statements
    Updated package import and removed comments.

--- TMessagesProj/src/main/java/tj/ Tajgram/messenger/Extra.java
+++ TMessagesProj/src/main/java/tj/ Tajgram/messenger/Extra.java
@@ -1,10 +1,8 @@
-package tj.Tajgram.messenger; // Суроғаи папкаи миллии нави ту
+package tj.Tajgram.messenger;
 
-import org.telegram.messenger.regular.BuildConfig; // Импорти мустақими Грэдли ту барои хондани калидҳо
+import org.telegram.messenger.BuildConfig;
 
 public class Extra {
-
-    // Калидҳои асосии амниятӣ, ки аз GitHub Secrets ва local.properties меоянд
     public static int APP_ID = BuildConfig.API_ID;
     public static String APP_HASH = BuildConfig.API_HASH;
     public static String APP_SHA256 = BuildConfig.SHA_256;

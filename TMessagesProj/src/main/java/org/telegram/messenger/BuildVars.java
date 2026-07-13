03063195d2bc6206ec2940d3eefa22b98c001423
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-07-13 12:01:17 +0300

    Update BuildVars with new configuration options

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -1,3 +1,10 @@
+/*
+ * This is the source code of Tajgram for Android v. 7.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Saidjon Tajgram, 2026
+ */
 
 package org.telegram.messenger;
 
@@ -11,14 +18,15 @@ import java.util.Objects;
 
 import tj.Tajgram.messenger.Extra;
 
-
 public class BuildVars {
     // --- [СИСТЕМАИ 1: АМНИЯТ ВА АВТОРИЗАЦИЯ] ---
+
     public static boolean FIREBASE_AUTH_PHONE_ENABLED = true;
 
     public static String SMS_VERIFICATION_PROVIDER = "google_firebase";
 
     public static boolean FREE_SMS_GATEWAY = true;
+
 
     // --- [СИСТЕМАИ 2: КАШЛОҚ ВА МОНЕТИЗАЦИЯ БО ФОИЗҲОИ НАВ] ---
 
@@ -89,7 +97,7 @@ public class BuildVars {
 
     public static boolean CUSTOM_VIP_GOLDEN_BADGE = true;
 
-    public static String APP_SHA256;
+     public static String APP_SHA256;
 
     public static boolean ANTI_PHISHING_URL_SHIELD = true; // Идея: Блоки автоматии силкаҳои спамӣ
 
@@ -184,6 +192,7 @@ public class BuildVars {
 
     public static boolean DEBUG_VERSION = BuildConfig.DEBUG_VERSION;
     public static void setupProxy() { ProxyAgent.smartLoad(); }
+    public static boolean LOGS_ENABLED = BuildConfig.DEBUG_VERSION;
     public static boolean DEBUG_PRIVATE_VERSION = BuildConfig.DEBUG_PRIVATE_VERSION;
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = true;
@@ -195,16 +204,13 @@ public class BuildVars {
     
     // SafetyNet key for Google Identity SDK, set it to empty to disable
     public static String SAFETYNET_KEY = "AIzaSyDKX59jtnkwyX-f_-SHVJF9H6IweVpXp74";
+    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
     public static String HUAWEI_STORE_URL = "https://appgallery.huawei.com/app/C101184875";
-    public static String GOOGLE_AUTH_CLIENT_ID = "811900975748-c1g3vv8ek67mbk8b03ejlgp8gv68qauv.apps.googleusercontent.com";
-    
+    public static String GOOGLE_AUTH_CLIENT_ID = "11900975748-c1g3vv8ek67mbk8b03ejlgp8gv68qauv.apps.googleusercontent.com";
+
     public static String HUAWEI_APP_ID = "101184875";
 
-    public static boolean LOGS_ENABLED; 
-    public static String PLAYSTORE_APP_URL;
-
-
-    // You can use this flag to disable Google Play Billing ( If you're making fork and want it to be in Google Play)
+    // You can use this flag to disable Google Play Billing (If you're making fork and want it to be in Google Play)
     public static boolean IS_BILLING_UNAVAILABLE = false;
 
     // works only on official app ids, disable on your forks
@@ -226,12 +232,11 @@ public class BuildVars {
         }
     }
 
-    
     static {
         APP_ID = Extra.APP_ID;
         APP_HASH = Extra.APP_HASH;
         APP_SHA256 = Extra.APP_SHA256;
-        PLAYSTORE_APP_URL = "https://t.me/tajgramTips"; 
+        PLAYSTORE_APP_URL = "https://t.me/tajgramTips";
         if (ApplicationLoader.applicationContext != null) {
             SharedPreferences sharedPreferences = ApplicationLoader.applicationContext.getSharedPreferences("systemConfig", Context.MODE_PRIVATE);
             LOGS_ENABLED = DEBUG_VERSION || sharedPreferences.getBoolean("logsEnabled", DEBUG_VERSION);
@@ -246,7 +251,6 @@ public class BuildVars {
             }
         }
     }
-
 
     public static boolean useInvoiceBilling() {
         return BillingController.billingClientEmpty || DEBUG_VERSION && false || ApplicationLoader.isStandaloneBuild() || isBetaApp() && false || isHuaweiStoreApp() || hasDirectCurrency();

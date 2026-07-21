e1b5c2d2ac88f92c00dff19108678a01f846dbd9
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-07-21 19:20:12 +0300

    Update GOOGLE_AUTH_CLIENT_ID for authentication

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -37,7 +37,7 @@ public class BuildVars {
     public static String SAFETYNET_KEY = "";
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
     public static String HUAWEI_STORE_URL = "https://appgallery.huawei.com/app/C101184875";
-    public static String GOOGLE_AUTH_CLIENT_ID = "760348033671-81kmi3pi84p11ub8hp9a1funsv0rn2p9.apps.googleusercontent.com";
+    public static String GOOGLE_AUTH_CLIENT_ID = "11900975748-c1g3vv8ek67mbk8b03ejlgp8gv68qauv.apps.googleusercontent.com";
 
     public static String HUAWEI_APP_ID = "101184875";
 

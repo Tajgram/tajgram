5c0c872dfb0dbbcd937c0119ec633115740cfbc0
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-07-27 21:39:28 +0300

    Rename APP_SHA256 to SHA256 in Extra.java

--- TMessagesProj/src/main/java/tj/ Tajgram/messenger/Extra.java
+++ TMessagesProj/src/main/java/tj/ Tajgram/messenger/Extra.java
@@ -5,7 +5,7 @@ import org.telegram.messenger.BuildConfig;
 public class Extra {
     public static int APP_ID = BuildConfig.APP_ID;
     public static String APP_HASH = BuildConfig.APP_HASH;
-    public static String APP_SHA256 = BuildConfig.SHA256;
+    public static String SHA256 = BuildConfig.SHA256;
     public static String CRYPTO_BOT_TOKEN = BuildConfig.CRYPTO_BOT_TOKEN;
 
     // Нишон додани намуди билд (Debug ё Release) барои ядрои барнома

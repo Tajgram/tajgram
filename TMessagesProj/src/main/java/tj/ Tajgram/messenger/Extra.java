7f469b943590daa6ea4df12ed08d59b5d48d770f
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-07-27 21:32:04 +0300

    Update APP_ID and APP_HASH from BuildConfig

--- TMessagesProj/src/main/java/tj/ Tajgram/messenger/Extra.java
+++ TMessagesProj/src/main/java/tj/ Tajgram/messenger/Extra.java
@@ -3,17 +3,18 @@ package tj.Tajgram.messenger;
 import org.telegram.messenger.BuildConfig;
 
 public class Extra {
-    public static int APP_ID = BuildConfig.API_ID;
-    public static String APP_HASH = BuildConfig.API_HASH;
+    public static int APP_ID = BuildConfig.APP_ID;
+    public static String APP_HASH = BuildConfig.APP_HASH;
     public static String APP_SHA256 = BuildConfig.SHA256;
+    public static String CRYPTO_BOT_TOKEN = BuildConfig.CRYPTO_BOT_TOKEN;
 
     // Нишон додани намуди билд (Debug ё Release) барои ядрои барнома
     public static boolean isDirectApp() {
         return "release".equals(BuildConfig.BUILD_TYPE) || "debug".equals(BuildConfig.BUILD_TYPE);
-        }
+    }
 
     // Санҷиши аслияти калид
-  public static boolean isKeyValid() {
+    public static boolean isKeyValid() {
         return APP_ID != 0 && APP_HASH != null && !APP_HASH.isEmpty();
     }
 }

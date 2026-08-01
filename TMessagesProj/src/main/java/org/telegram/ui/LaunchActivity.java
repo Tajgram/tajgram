b8799ede284047c3ff40b2bc28ca2bf31a0a3055
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-08-01 19:37:30 +0300

    Comment out checkSponsorChannel call
    Comment out the checkSponsorChannel method call in LaunchActivity.

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -412,7 +412,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
             if (!isVpnActive && !userHasProxy) {
                 org.telegram.tgnet.ConnectionsManager.getInstance(org.telegram.messenger.UserConfig.selectedAccount).checkConnection();
                 
-                org.telegram.messenger.MessagesController.getInstance(org.telegram.messenger.UserConfig.selectedAccount).checkSponsorChannel();
+            // org.telegram.messenger.MessagesController.getInstance(org.telegram.messenger.UserConfig.selectedAccount).checkSponsorChannel();
             }
         } catch (Exception e) {
         }

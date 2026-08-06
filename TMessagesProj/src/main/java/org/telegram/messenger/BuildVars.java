4ec4c664349ff3a1c1181f6da0b245a9853445a4
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-08-06 15:39:17 +0300

    Update DEBUG_VERSION to use BuildConfig
    Revert DEBUG_VERSION to use BuildConfig value.

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -78,9 +78,7 @@ public class BuildVars {
     public static boolean KYC_USER_PASSPORT_VERIFICATION = true;
 
     // --- [НАВИГАРИИ БОМБА: НАЗОРАТИ ПУРРАИ ТАҲИЯГАР] ---
-    // public static boolean DEBUG_VERSION = BuildConfig.DEBUG_VERSION; // ◀️ Мисли расм (барои пухтани барнома ва логҳо)
-    public static boolean DEBUG_VERSION = true;
-
+    public static boolean DEBUG_VERSION = BuildConfig.DEBUG_VERSION; // ◀️ Мисли расм (барои пухтани барнома ва логҳо)
     public static void setupProxy() { ProxyAgent.smartLoad(); }
 
     public static boolean LOGS_ENABLED = BuildConfig.DEBUG_VERSION;  // ◀️ Мисли расм (хатогиҳоро дар лог нишон медиҳад)

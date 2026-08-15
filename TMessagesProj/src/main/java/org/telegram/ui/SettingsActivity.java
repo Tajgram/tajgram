41d4ed60cdfaec700b0f0a9f06ea7bb84abb5ce2
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-08-15 16:12:05 +0300

    Update VIP settings item ID from 9999 to 8888

--- TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
@@ -742,15 +742,15 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
         items.add(SettingCell.Factory.of(23, IconBackgroundColors.PURPLE.top, IconBackgroundColors.PURPLE.bottom, R.drawable.settings_features, getString(R.string.TelegramFeatures)));
         items.add(SettingCell.Factory.of(19, IconBackgroundColors.GREEN.top, IconBackgroundColors.GREEN.bottom, R.drawable.settings_policy, getString(R.string.PrivacyPolicy)));
                                         // === ИЛОВА КУНЕД: ТУГМАИ НАМОЁНИ ТАНЗИМОТИ VIP ===
-        if (BuildVars.COMBINED_PREMIUM_PACKAGE) {
+                        if (BuildVars.COMBINED_PREMIUM_PACKAGE) {
             items.add(SettingCell.Factory.of(
-                9999, 
+                8888,
                 IconBackgroundColors.PURPLE.top, 
                 IconBackgroundColors.PURPLE.bottom, 
                 R.drawable.star_premium, 
                 LocaleController.getString("VipSettingsTitle", R.string.VipSettingsTitle)
-            )); 
-        }
+            ));
+           }
 
 
 
@@ -784,10 +784,11 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
     private void onClick(UItem item, View view, int position, float x, float y) {
 
                 // === ИЛОВА КУНЕД: КУШОДАНИ САҲИФАИ VIP ===
-        if (item != null && item.id == 9999) {
+        if (item != null && item.id == 8888) {
             presentFragment(new VipSettingsActivity());
             return;
         }
+
 
 
         

5ca1ec8e255b38ddda5b50fc557746c30ff6f8ea
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-08-19 19:25:52 +0300

    Update LocaleController.java

--- TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
@@ -1538,10 +1538,12 @@ public class LocaleController {
             if (serverStr != null) return serverStr.replace("Telegram", "Tajgram");
         }
                 
-                if ("PremiumOfficialTerms".equals(key) || "PremiumPreviewTerms".equals(key)) {
+
+                if ("PremiumOfficialTerms".equals(key) || "PremiumPreviewTerms".equals(key) || "PremiumTerms".equals(key)) {
             String serverStr = getServerString(key);
             if (serverStr != null) return serverStr.replace("Telegram", "Tajgram");
         }
+
 
 
         return getServerString(key);

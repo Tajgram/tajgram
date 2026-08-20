f7e6516b2cd9b404a630e039dafcead84848d8dd
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-08-20 09:15:34 +0300

    Update LocaleController.java

--- TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
@@ -1481,8 +1481,7 @@ public class LocaleController {
                          .replace("Telegram Stars", "Tajgram Stars")
                          .replace("Telegram", "Tajgram");
         if ("InviteText2".equals(key) && value != null) value = value.replace("Telegram", "Tajgram").replace("telegram", "tajgram");
-        if (key != null && (key.contains("Premium") || key.contains("Terms")) && value != null) value = value.replace("Telegram", "Tajgram").replace("telegram", "tajgram");
-
+        if ("PremiumSubscribeTerms".equals(key) && value != null) value = value.replace("Telegram Premium", "Tajgram Premium VIP").replace("Telegram", "Tajgram").replace("telegram", "tajgram");
 
         return value;
     }
@@ -1545,10 +1544,10 @@ public class LocaleController {
             if (serverStr != null) return serverStr.replace("Telegram", "Tajgram");
         }
 
-                if (key != null && (key.contains("Premium") || key.contains("Terms"))) {
-            String serverStr = getServerString(key);
-            if (serverStr != null) return serverStr.replace("Telegram", "Tajgram").replace("telegram", "tajgram");
+                 if ("PremiumSubscribeTerms".equals(key) && result != null) {
+            result = result.replace("Telegram Premium", "Tajgram Premium VIP").replace("Telegram", "Tajgram").replace("telegram", "tajgram");
         }
+  
 
 
         return getServerString(key);

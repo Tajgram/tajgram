cfcfd7b719c1784ee412c223997c4e98924b1294
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-08-23 02:09:51 +0300

    New

--- TMessagesProj/src/main/java/org/telegram/messenger/MediaDataController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MediaDataController.java
@@ -65,7 +65,6 @@ import org.telegram.tgnet.TLRPC;
 import org.telegram.tgnet.Vector;
 import org.telegram.tgnet.tl.TL_account;
 import org.telegram.tgnet.tl.TL_bots;
-import org.telegram.tgnet.tl.TL_ephemeral;
 import org.telegram.tgnet.tl.TL_iv;
 import org.telegram.tgnet.tl.TL_update;
 import org.telegram.ui.ActionBar.BaseFragment;
@@ -450,6 +449,12 @@ public class MediaDataController extends BaseController {
     }
 
     public TLRPC.TL_help_premiumPromo getPremiumPromo() {
+        
+        if (premiumPromo != null && premiumPromo.status_text != null && !premiumPromo.status_text.contains("Tajgram")) premiumPromo.status_text = premiumPromo.status_text.replace("Telegram", "Tajgram").replace("telegram", "tajgram");
+
+        if (premiumPromo != null && "tg".equals(org.telegram.messenger.LocaleController.getInstance().getCurrentLocaleInfo().shortName)) premiumPromo.status_text = "Бо обуна шудан ба Tajgram Premium шумо ба Tajgram Terms of Service ва Privacy Policy розигӣ медиҳед.";
+
+
         return premiumPromo;
     }
 
@@ -687,6 +692,12 @@ public class MediaDataController extends BaseController {
     public void processLoadedPremiumPromo(TLRPC.TL_help_premiumPromo premiumPromo, int date, boolean cache) {
         if (premiumPromo != null) {
             this.premiumPromo = premiumPromo;
+            
+            if (premiumPromo != null && premiumPromo.status_text != null && !premiumPromo.status_text.contains("Tajgram")) premiumPromo.status_text = premiumPromo.status_text.replace("Telegram", "Tajgram").replace("telegram", "tajgram");
+
+            if (premiumPromo != null && "tg".equals(org.telegram.messenger.LocaleController.getInstance().getCurrentLocaleInfo().shortName)) premiumPromo.status_text = "Бо обуна шудан ба Tajgram Premium шумо ба Tajgram Terms of Service ва Privacy Policy розигӣ медиҳед.";
+
+
             premiumPromoUpdateDate = date;
             getMessagesController().putUsers(premiumPromo.users, cache);
             AndroidUtilities.runOnUIThread(() -> getNotificationCenter().postNotificationName(NotificationCenter.premiumPromoUpdated));
@@ -6583,9 +6594,9 @@ public class MediaDataController extends BaseController {
                             }
                         }
                         if (!ephemeralIds.isEmpty()) {
-                            ArrayList<TL_ephemeral.EphemeralMessage> ephemeralMessages = getMessagesStorage().getEphemeralMessagesInternal(dialogId, ephemeralIds);
+                            ArrayList<TLRPC.EphemeralMessage> ephemeralMessages = getMessagesStorage().getEphemeralMessagesInternal(dialogId, ephemeralIds);
                             if (ephemeralMessages != null) {
-                                for (TL_ephemeral.EphemeralMessage ephemeralMessage : ephemeralMessages) {
+                                for (TLRPC.EphemeralMessage ephemeralMessage : ephemeralMessages) {
                                     TLRPC.Message convetedEphemeralMessage = EphemeralMessagesHelper.convertEphemeralToFakeDefault(ephemeralMessage);
                                     MessagesStorage.addUsersAndChatsFromMessage(convetedEphemeralMessage, usersToLoad, chatsToLoad, null);
                                     result.add(convetedEphemeralMessage);

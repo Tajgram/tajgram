a656a287085a1f8411c98934e388b36332e60332
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-08-24 20:39:53 +0300

    Update Latex.java

--- TMessagesProj/src/main/java/org/telegram/ui/iv/Latex.java
+++ TMessagesProj/src/main/java/org/telegram/ui/iv/Latex.java
@@ -9,8 +9,8 @@ import androidx.annotation.Nullable;
 import org.telegram.messenger.ApplicationLoader;
 import org.telegram.messenger.FileLog;
 
-// import ru.noties.jlatexmath.JLatexMathAndroid;
-// import ru.noties.jlatexmath.JLatexMathDrawable;
+ import ru.noties.jlatexmath.JLatexMathAndroid;
+ import ru.noties.jlatexmath.JLatexMathDrawable;
 
 public final class Latex {
 
@@ -50,30 +50,30 @@ public final class Latex {
         }
         try {
             ensureInitialized();
-           // final JLatexMathDrawable drawable =
-           //     JLatexMathDrawable.builder(source)
-           //         .textSize(textSizePx)
-           //         .build();
-           // final int w = drawable.getIntrinsicWidth();
-           // final int h = drawable.getIntrinsicHeight();
-           // if (w <= 0 || h <= 0) {
-           //     return null;
-          //  }
-         //   final Bitmap bm = Bitmap.createBitmap(w, h, Bitmap.Config.ALPHA_8);
-         //   drawable.setBounds(0, 0, w, h);
-         //   drawable.draw(new Canvas(bm));
-         //   int depth = 0;
-        //    if (needDepth) {
-       //         try {
-          //          depth = drawable.icon().getIconDepth();
-          //      } catch (Throwable e) {
-         //           FileLog.e(e);
-       //         }
-       //     }
-       //     return new Latex(bm, w, h, depth);
+            final JLatexMathDrawable drawable =
+                JLatexMathDrawable.builder(source)
+                    .textSize(textSizePx)
+                    .build();
+            final int w = drawable.getIntrinsicWidth();
+            final int h = drawable.getIntrinsicHeight();
+            if (w <= 0 || h <= 0) {
+                return null;
+            }
+            final Bitmap bm = Bitmap.createBitmap(w, h, Bitmap.Config.ALPHA_8);
+            drawable.setBounds(0, 0, w, h);
+            drawable.draw(new Canvas(bm));
+            int depth = 0;
+            if (needDepth) {
+                try {
+                    depth = drawable.icon().getIconDepth();
+                } catch (Throwable e) {
+                    FileLog.e(e);
+                }
+            }
+            return new Latex(bm, w, h, depth);
         } catch (Throwable e) {
             FileLog.e(e);
             return null;
         }
-   // }
+    }
 }

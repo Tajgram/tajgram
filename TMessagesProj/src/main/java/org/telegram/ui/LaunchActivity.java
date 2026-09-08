27745b02baef21773950fa34c97fcf51db271800
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-09-08 21:46:36 +0300

    Refactor error handling in LaunchActivity
    Comment out try-catch block in onCreate method.

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -392,8 +392,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-           try {
-
+  //         try {
 // android.net.ConnectivityManager cm = (android.net.ConnectivityManager) org.telegram.messenger.ApplicationLoader.applicationContext.getSystemService(android.content.Context.CONNECTIVITY_SERVICE);
 // boolean isVpnActive = false;
 // if (cm != null) {

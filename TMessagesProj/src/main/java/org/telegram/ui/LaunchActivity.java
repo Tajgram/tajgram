c766b1bbdaffbb3347b247fc78c1f68295059153
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-09-09 08:07:27 +0300

    Fix typo in getRootAnimatedInsetsListener method

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -374,7 +374,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
             systemBlurEnabled = aBoolean;
         }
     };
-
+    
     private FlagSecureReason flagSecureReason;
     private final LiteMode.BatteryReceiver batteryReceiver = new LiteMode.BatteryReceiver();
     private WindowAnimatedInsetsProvider rootAnimatedInsetsListener;

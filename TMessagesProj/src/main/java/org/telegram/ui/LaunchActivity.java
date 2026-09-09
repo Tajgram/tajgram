d10784dbbca03eed3ec4d25f6af713d404fba925
Tajgram <79911205+Tajgram@users.noreply.github.com>
2026-09-09 19:50:25 +0300

    Update LaunchActivity.java

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -533,7 +533,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                             }
                         }
                     } catch (Exception ignored) {
-                    } finaly {
+                    } finally {
                         if (reader != null) {
                             try { reader.close(); } catch (Exception ignored) {}
                         }
